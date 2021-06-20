ca3e20bf378bef94386773be0192cf651c1bbff5
Jacob Chikwanda <jaynjaycob@gmail.com>
2021-06-20 09:23:06 -0700

    3rd commit

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,13 +1,13 @@
 #include <stdlib.h>
 #include <stdio.h>
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success)
+/**                                                                                                                  
+ * main - Entry point                                                                                                
+ *                                                                                                                   
+ * Return: Always 0 (Success)                                                                                        
  */
 int main(void)
 {
-for (int a = 97; a <= 122; a++)
+for (int a = 'a'; a <= 'z'; a++)
 putchar(a);
 putchar('\n');
 return (0);

c0b75829703227b95d9b966623880cbef34a6af3
Jacob Chikwanda <jaynjaycob@gmail.com>
2021-06-22 10:47:30 -0700

    commit 2

--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -1,6 +1,12 @@
 #ifndef _HOLBERTON_H
 #define _HOLBERTON_H
-#include <unistd.h>
+
+/**
+ * print_alphabet - prints the alphabet
+ *
+ * Return: Returns 0 Success
+ */
+void print_alphabet(void);
 
 /**
  * _putchar - writes the character c to stdout
@@ -10,12 +16,5 @@
  * On error, -1 is returned, and errno is set appropriately.
  */
 int _putchar(char c);
-
-/**
- * print_alphabet - prints the alphabet
- *
- * Return: Returns 0 Success
- */
-void print_alphabet(void);
 
 #endif

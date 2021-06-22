00c512577d3a106fd2cec22d7d04e99487ac3aac
Jacob Chikwanda <jaynjaycob@gmail.com>
2021-06-22 08:47:51 -0700

    commit 1

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -8,14 +8,5 @@
 int main(void)
 {
 _putchar('H');
-_putchar('o');
-_putchar('l');
-_putchar('b');
-_putchar('e');
-_putchar('r');
-_putchar('t');
-_putchar('o');
-_putchar('n');
-_putchar('\n');
 return (0);
 }

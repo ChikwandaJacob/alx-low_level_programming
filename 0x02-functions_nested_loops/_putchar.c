c9393b0b1789fb51fb2c26d21e2fd150f44a60f6
Jacob Chikwanda <jaynjaycob@gmail.com>
2021-06-22 10:17:08 -0700

    commit 2

--- 0x02-functions_nested_loops/_putchar.c
+++ 0x02-functions_nested_loops/_putchar.c
@@ -12,3 +12,16 @@ int _putchar(char c)
 {
 	return (write(1, &c, 1));
 }
+
+/**
+ * print_alphabet - prints the alphabet.
+ *
+ * Return: returns nothing.
+ */
+void print_alphabet(void)
+{
+char a;
+for (a = 'a'; a <= 'z'; a++)
+putchar(a);
+putchar('\n');
+}

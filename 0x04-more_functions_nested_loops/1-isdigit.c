50165c299e71f6df97e77fb100b0a7e938bd7189
Jacob Chikwanda <jaynjaycob@gmail.com>
2021-06-24 07:28:45 -0700

    commit 3

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -8,12 +8,7 @@
  */
 int _isdigit(int c)
 {
-int is_digit = 0, i;
-for (i = 0; i < 10; i++)
-if (i == c)
-is_digit = 1;
-if (is_digit == 1)
-return (1);
-else
-return (0);
+	if (c >= 0 || c <= 9)
+		return (1);
+	return (0);
 }

4704f1d23ab8fed8649e60c18622b75893603047
Jacob Chikwanda <jaynjaycob@gmail.com>
2021-06-24 11:48:56 -0700

    commit 5

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -8,12 +8,13 @@
  */
 void print_most_numbers(void)
 {
-char i;
-for (i = '0'; i <= '9'; i++)
-{
-if (i == '2' || i == '4')
-continue;
-putchar(i);
-}
-putchar('\n');
+	char i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		if (i == '2' || i == '4')
+			continue;
+		_putchar(i);
+	}
+	_putchar('\n');
 }

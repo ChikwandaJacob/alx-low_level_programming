ccef2dd1ab316179db6a4cfbdbc71a08ccfa4648
Jacob Chikwanda <jaynjaycob@gmail.com>
2021-06-25 02:26:17 -0700

    commit 14

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,19 +1,6 @@
 #include "holberton.h"
 
 /**
- * printNumbers - prints all values of an interher
- * @n: number to be printed
- *
- * Return: returns nothing.
- */
-void printNumbers(int n)
-{
-	if (n >= 10)
-		printNumbers(n / 10);
-	_putchar((n % 10) + '0');
-}
-
-/**
  * print_number - prints an integer.
  * @n: number to be printed
  *
@@ -26,5 +13,7 @@ void print_number(int n)
 		n = n * -1;
 		_putchar('-');
 	}
-	printNumbers(n);
+	if (n >= 10)
+		print_number(n / 10);
+	_putchar((n % 10) + '0');
 }

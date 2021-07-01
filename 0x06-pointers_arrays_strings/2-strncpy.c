3413fae608b3a6872648290320e8a3ef30cf9838
Jacob Chikwanda <jaynjaycob@gmail.com>
2021-07-01 02:51:37 -0700

    _strncpy function created

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -9,11 +9,11 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-	int i, j;
+	int i = 0;
 
-	for (i = 0; dest[i] != '\0'; i++)
-		;
-	for (j = 0; j < n; j++)
-		dest[j] = src[j];
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[i] = src[i];
+	for ( ; i < n; i++)
+		dest[i] = '\0';
 	return (dest);
 }

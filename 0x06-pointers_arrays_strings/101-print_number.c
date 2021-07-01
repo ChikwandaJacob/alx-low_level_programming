856ec9f815dfe2e3f1ac120a4d0b6813b1210bae
Jacob Chikwanda <jaynjaycob@gmail.com>
2021-07-01 14:46:38 -0700

    function to print integers created

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -7,14 +7,21 @@
  */
 void print_number(int n)
 {
-	if (n < 0)
+	if ((n / 10) != 0)
+	{
+		print_number(n / 10);
+		if (n > 0)
+			_putchar((n % 10) + '0');
+		else
+			_putchar((-n % 10) + '0');
+	}
+	else if ((n / 10 == 0) && (n % 10 != 0) && (n > 0))
+	{
+		_putchar(n % 10 + '0');
+	}
+	else if ((n / 10 == 0) && (n%10 != 0) && (n <= 0))
 	{
 		_putchar('-');
-		n *= -1;
+		_putchar(-n % 10 + '0');
 	}
-
-	if ((n / 10) > 0)
-		print_number(n / 10);
-
-	_putchar((n % 10) + '0');
 }

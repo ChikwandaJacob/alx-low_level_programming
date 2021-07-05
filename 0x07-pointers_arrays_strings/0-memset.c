9a1d4a4c5080e286d41157ed907ae153ed55ee90
Jacob Chikwanda <jaynjaycob@gmail.com>
2021-07-05 08:47:18 -0700

    _memset updated

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -5,12 +5,13 @@
  * @s: array
  * @b: byte to fill array memory ares.
  * @n: array size
+ * Return: returns pointer
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-        int i = 0, max = n;
+	int i = 0, max = n;
 
 	for ( ; i < max; i++)
 		s[i] = b;
-	return s;
+	return (s);
 }

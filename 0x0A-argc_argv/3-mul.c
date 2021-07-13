e729af7a35d0df3d39e4491be53f1634f6b345f1
Yakufaith <jaynjaycob@gmail.com>
2021-07-13 11:34:37 +0200

    3-mul.c program multiplication process fixed

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -11,8 +11,14 @@
  */
 int main(int argc, char *argv[])
 {
+	int i, product = 1;
+
 	if (argc > 1)
-		printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+	{
+		for (i = 1; i < argc; i++)
+			product *= atoi(argv[i]);
+		printf("%d\n", product);
+	}
 	else
 	{
 		printf("Error\n");

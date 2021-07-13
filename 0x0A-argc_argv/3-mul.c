afff744575f69277959b9e7725735c19cad1b153
Yakufaith <jaynjaycob@gmail.com>
2021-07-13 11:26:05 +0200

    3-mul.c program created

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - programs main function
+ * @argc: number of things typed in command line
+ * @argv: array of string values for typed things
+ * Return: returns the name of the program followed by a new line.
+ * If you rename the program, it will print the new name,
+ * without having to compile it again
+ */
+int main(int argc, char *argv[])
+{
+	if (argc > 1)
+		printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+	return (0);
+}

a46fc7144ec0539c5e674cfbc860269a76ea71c1
Yakufaith <jaynjaycob@gmail.com>
2021-07-14 19:56:14 +0200

    0-create_array.c return condition fixed when size = 0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -6,14 +6,15 @@
  * specific character.
  * @size: size of array to be created
  * @c: character to initialize array with.
- * Return: Returns a pointer to the array, or NULL if it fails
+ * Return: Returns a pointer to the array, or NULL if it fails.
+ * Also, function returns 0 if @size is zero.
  */
 char *create_array(unsigned int size, char c)
 {
 	char *ptr = (char *) malloc(size * sizeof(int));
 	unsigned int i;
 
-	if (ptr == NULL)
+	if (ptr == NULL || size == 0)
 		return (NULL);
 	for (i = 0; i < size; i++)
 		ptr[i] = c;

b9d79b5fcb085128b4d71131768a68512920b95c
Yakufaith <jaynjaycob@gmail.com>
2021-07-14 22:50:16 +0200

    3-alloc_grid.c updated

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -24,6 +24,9 @@ int **alloc_grid(int width, int height)
 	for (i = 0; i < height; i++)
 		ptr[i] = (int *) malloc(width * sizeof(int));
 
+	if (ptr == NULL)
+		return (NULL);
+
 	for (i = 0; i < height; i++)
 		for (j = 0; j < width; j++)
 			ptr[i][j] = 0;

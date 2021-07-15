de54a796da7c0e145f04b1e26ddb73ef91763418
Yakufaith <jaynjaycob@gmail.com>
2021-07-16 01:32:34 +0200

    all files created

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -3,22 +3,32 @@
 
 /**
  * _realloc - Reallocates a memory block using malloc and free.
- *          - Contents will be copied to the newly allocated space,
- *            in the range from the start of ptr up to the minimum
- *            of the old and new sizes
- * @ptr: pointer to the memory previously allocated
- * @old_size: the size, in bytes, of the allocated space for ptr
- * @new_size: the new size, in bytes of the new memory block
- * Return: pointer to newly allocated block.
+ * @ptr: A pointer to the memory previously allocated.
+ * @old_size: The size in bytes of the allocated space for ptr.
+ * @new_size: The size in bytes for the new memory block.
+ *
+ * Return: If new_size == old_size - ptr.
+ *         If new_size == 0 and ptr is not NULL - NULL.
+ *         Otherwise - a pointer to the reallocated memory block.
  */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	unsigned int i, size;
-	void *new_ptr;
-	char *new_alloc, *temp_data;
+	void *mem;
+	char *ptr_copy, *filler;
+	unsigned int index;
 
 	if (new_size == old_size)
 		return (ptr);
+
+	if (ptr == NULL)
+	{
+		mem = malloc(new_size);
+
+		if (mem == NULL)
+			return (NULL);
+
+		return (mem);
+	}
 
 	if (new_size == 0 && ptr != NULL)
 	{
@@ -26,28 +36,20 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		return (NULL);
 	}
 
-	if (new_size > old_size)
-		size = new_size;
-	else
-		size = old_size + new_size;
+	ptr_copy = ptr;
+	mem = malloc(sizeof(*ptr_copy) * new_size);
 
-	new_ptr = malloc(size + 1);
-
-	if (new_ptr == NULL)
-		return (NULL);
-
-	temp_data = ptr;
-	new_alloc = new_ptr;
-
-	for (i = 0; i < size; i++)
+	if (mem == NULL)
 	{
-		if (i < old_size)
-			new_alloc[i] = temp_data[i];
-		else
-			new_alloc[i] = '\0';
+		free(ptr);
+		return (NULL);
 	}
 
-	free(ptr);
+	filler = mem;
 
-	return (new_alloc);
+	for (index = 0; index < old_size && index < new_size; index++)
+		filler[index] = *ptr_copy++;
+
+	free(ptr);
+	return (mem);
 }

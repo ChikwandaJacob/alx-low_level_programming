1a06e61f41ccf364de52836eefd8186c72d5744f
Yakufaith <jaynjaycob@gmail.com>
2021-07-15 22:14:42 +0200

    2-calloc.c created

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -12,15 +12,19 @@
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	void *p;
+	unsigned int i = 0;
+	char *p;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 
-	p = calloc(nmemb, size);
+	p = malloc(nmemb * size);
 
 	if (p == NULL)
 		return (NULL);
+
+	for ( ; i < nmemb; i++)
+		p[i] = '0';
 
 	return (p);
 }

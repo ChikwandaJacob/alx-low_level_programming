58eafe0d440a581b9d736b98551b56965ae7ddd5
Yakufaith <jaynjaycob@gmail.com>
2021-07-19 14:29:53 +0200

    This file defines a new type struct named dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,15 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - defines a dog structure
+ * @name: name of the dog
+ * @owner: name of the owner of the dog
+ * @age: age of the dog
+ *
+ * Description: This structure helps store some useful information
+ *              about a dog i.e. its name, the owner and its age.
+ */
 struct dog
 {
 	char *name, *owner;

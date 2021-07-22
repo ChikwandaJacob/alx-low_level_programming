0a5bf7ae84de152a8b8f77319493aa2c0b6d100d
Yakufaith <jaynjaycob@gmail.com>
2021-07-22 21:22:42 +0200

    Function to print strings updated

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -3,20 +3,6 @@
 #include <stdio.h>
 
 /**
- * is_null - checks whether given string is null.
- * @s: string to be checked.
- *
- * Return: (nil) if string is empty
- *         returns @s if not empty.
- */
-char *is_null(char *s)
-{
-	if (s != NULL)
-		return (s);
-	return ("nill");
-}
-
-/**
  * print_strings - prints numbers followed by a new line.
  * @separator: string that will separate the numbers if it is given.
  * @n: number of strings to be printed.
@@ -26,31 +12,28 @@ char *is_null(char *s)
 void print_strings(const char *separator, const unsigned int n, ...)
 {
 	unsigned int i = 0;
-	const char *s = separator;
+	const char *sep = separator;
 	char *str;
 
 	va_list list;
 
-	if (n != 0)
-	{
-		/*Initializing List*/
-		va_start(list, n);
+        /*Initializing List*/
+        va_start(list, n);
 
-		for ( ; i < n; i++)
-		{
-			str = is_null((va_arg(list, char *)));
+        for ( ; i < n; i++)
+        {
+	        str = va_arg(list, char *);
 
-			if (i < n - 1)
-			{
-				if (s != NULL)
-					printf("%s%s", str, s);
-				else
-					printf("%s", str);
-			}
-			else
-				printf("%s\n", str);
-		}
+	        if (str == NULL)
+		        printf("(nill)");
+	        else
+	        {
+		        printf("%s", str);
+		        if (sep != NULL && i < n - 1)
+			        printf("%s", sep);
+	        }
+        }
 
-		va_end(list);
-	}
+	printf("\n");
+        va_end(list);
 }

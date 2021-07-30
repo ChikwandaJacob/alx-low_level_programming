79bcd73376271ab93295c6dd156a8156495882bd
Yakufaith <jaynjaycob@gmail.com>
2021-07-30 17:08:07 +0200

    Function to print singly linked list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -6,12 +6,13 @@
  * print_list - prints all the elements of a list_t list.
  * @h: head structure
  *
+ * Return: total number of nodes in a list.
  */
 size_t print_list(const list_t *h)
 {
 	unsigned int len = 0;
 
-	while(h != NULL)
+	while (h != NULL)
 	{
 		len++;
 		if (h->str == NULL)

61507bf342845f860653e29e14f4f45c2cea0ee5
Yakufaith <jaynjaycob@gmail.com>
2021-08-03 12:14:12 +0200

    logic to insert node in list updated

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -12,7 +12,7 @@
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	listint_t *new_node = malloc(sizeof(listint_t));
-	listint_t *temp = *head;
+	listint_t *temp = *head, *next;
 	unsigned int i = 0;
 
 	if (!*head)
@@ -20,18 +20,22 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	new_node->n = n;
 
-	if (idx == 0)
+        if (idx == 0)
 	{
 		new_node->next = temp;
 		*head = new_node;
 		return (*head);
 	}
 
-	for ( ; i < idx - 1; i++)
+	for ( ;temp != NULL && i < idx - 1; i++)
 		temp = temp->next;
 
-	new_node->next = temp->next;
-	temp->next = new_node;
+	if (temp == NULL || temp->next == NULL)
+		return (NULL);
 
-	return (temp);
+	next = temp->next;
+	temp->next = new_node;
+	new_node->next = next;
+
+	return (new_node);
 }

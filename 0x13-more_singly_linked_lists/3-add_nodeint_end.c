1a69229128b150f8d0816990d33a1d6e52af2baf
Yakufaith <jaynjaycob@gmail.com>
2021-08-03 11:46:52 +0200

    Fil;e has being updated. function description included

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 /**
- * add_nodeint - adds a new node at the end of a listint_t list
+ * add_nodeint_end - adds a new node at the end of a listint_t list
  * @head: pointer reference to the head node
  * @n: value to be added along
  *

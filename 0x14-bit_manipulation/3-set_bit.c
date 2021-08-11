97b7ed106b44e2413dd7400bbf75a462b9746f78
Yakufaith <jaynjaycob@gmail.com>
2021-08-11 16:37:59 +0200

    Functions sets given bit position to 1

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -9,6 +9,12 @@
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
+	unsigned long int init_val = *n;
+
 	*n = *n | (1 << index);
-	return (*n);
+
+	if (*n == init_val)
+		return (-1);
+
+	return (1);
 }

972bd12595cb27892c7c5604c9fe310a092d512d
Yakufaith <jaynjaycob@gmail.com>
2021-08-11 17:37:36 +0200

    Functions sets given bit position to 1

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -1,27 +1,6 @@
 #include "main.h"
 
 /**
- * get_bit_length - gets the length of bits in decimal
- * @num: number whose bits are to be calculated.
- *
- * Return: numbr of bits.
- */
-unsigned long int get_bit_length(unsigned long int num)
-{
-	unsigned long int i = 0;
-
-	while (num != 0)
-	{
-		if (i == 0)
-			i++;
-		num /= 2;
-		i++;
-	}
-
-	return (i);
-}
-
-/**
  * set_bit - gets bit at a given index.
  * @n: number containing bits
  * @index: index of bit
@@ -30,17 +9,9 @@ unsigned long int get_bit_length(unsigned long int num)
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	unsigned long int init_len = get_bit_length(*n), fin_len, temp = *n;
-
-	*n = *n | (1 << index);
-
-	fin_len = get_bit_length(*n);
-
-	if (fin_len != init_len && temp != 0)
-	{
-		*n = temp;
+	if (index)
+		*n = *n | (1 << index);
+	else
 		return (-1);
-	}
-
 	return (1);
 }

808b65abc948c06ba3ea82e059b6abaecea337e8
Yakufaith <jaynjaycob@gmail.com>
2021-08-13 20:33:48 +0200

    File contains a function that creates a new file and adds data to it. If file exits function truncates the file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,26 +10,25 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-        int fd = open(filename, O_WRONLY| O_CREAT | O_TRUNC, 0600), i = 0;
-        size_t content_length = 0;
+	int fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600), i = 0;
+	size_t content_length = 0;
 
-        if (fd == -1)
-                return (-1);
+	if (fd == -1)
+		return (-1);
 
-        if (!filename)
-                return (-1);
+	if (!filename)
+		return (-1);
 
-        if (text_content)
-        {
-                for ( ; text_content[i] != '\0'; i++, content_length++)
-                        ;
+	if (text_content)
+	{
+		for ( ; text_content[i] != '\0'; i++, content_length++)
+			;
 
-                if (write(fd, text_content, content_length) == -1)
-                        return (-1);
+		if (write(fd, text_content, content_length) == -1)
+			return (-1);
 
-                return (1);
-        }
+		return (1);
+	}
 
-        return (1);
-
+	return (1);
 }

c6b0bcd65e961ad9d42ee6aad71d2085cdd7351c
Yakufaith <jaynjaycob@gmail.com>
2021-08-13 23:46:07 +0200

    Program copies contents of 1 file to another just like the cp command

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+
+/**
+ * main - main function of the file
+ * @argc: number of command line arguments
+ * @argv: string values of command line arguments
+ *
+ * Return: 1 on success.
+ */
+int main(int argc, char **argv)
+{
+	ssize_t fd = open(argv[1], O_RDONLY), bytes_read;
+
+	char buf[1024];
+
+	if (argc != 3)
+	{
+		dprintf(2, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+
+	bytes_read = read(fd, buf, 1024);
+
+	if (bytes_read == -1 || fd == -1)
+	{
+		dprintf(2, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+
+	if (close(fd) == -1)
+	{
+		dprintf(2, "Error: Can't close fd %li\n", fd);
+		exit(100);
+	}
+
+	fd = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+
+	if (fd == -1 || write(fd, buf, bytes_read) == -1)
+	{
+		dprintf(2, "Error: Can't write to %s\n", argv[1]);
+		exit(98);
+	}
+
+	if (close(fd) == -1)
+	{
+		dprintf(2, "Error: Can't close fd %li\n", fd);
+		exit(100);
+	}
+
+	return (0);
+}

9e2c99b975255fb01e86a396cc9eb8293f72be73
Yakufaith <jaynjaycob@gmail.com>
2021-08-13 16:18:08 +0200

    File contains function defintion of method to read contents of file

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -13,15 +13,18 @@
  *         if write fails or does not write the expected amount of
  *         bytes, return 0
  */
- ssize_t read_textfile(const char *filename, size_t letters)
- {
+ssize_t read_textfile(const char *filename, size_t letters)
+{
 	char *buf = malloc(sizeof(char) * letters);
 	int fd;
-        ssize_t bytes_read, bytes_written;
+	ssize_t bytes_read, bytes_written;
+
+	if (!filename)
+		return (0);
 
 	fd = open(filename, O_RDONLY);
 
-	if (fd == -1)
+	if (fd == -1 || !buf)
 		return (0);
 
 	bytes_read = read(fd, buf, letters);
@@ -31,7 +34,7 @@
 	if (bytes_read == -1)
 		return (0);
 
-        bytes_written = write(1, buf, bytes_read);
+	bytes_written = write(1, buf, bytes_read);
 
 	if (bytes_written == -1 || bytes_written != bytes_read)
 		return (0);
@@ -39,4 +42,4 @@
 	free(buf);
 
 	return (bytes_written);
- }
+}

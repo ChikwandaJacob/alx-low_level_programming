1082f8b0287d15982e148c738e17fd4970386b74
Yakufaith <jaynjaycob@gmail.com>
2021-08-13 16:06:47 +0200

    File contains function defintion of method to read contents of file

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,42 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * read_textfile -  reads a text file and prints it to the POSIX
+ *                  standard output.
+ * @filename: name of the file to be read
+ * @letters: number of letters function should read and print
+ *
+ * Return: actual number of letters it could read and print
+ *         if the file can not be opened or read, return 0
+ *         if filename is NULL return 0
+ *         if write fails or does not write the expected amount of
+ *         bytes, return 0
+ */
+ ssize_t read_textfile(const char *filename, size_t letters)
+ {
+	char *buf = malloc(sizeof(char) * letters);
+	int fd;
+        ssize_t bytes_read, bytes_written;
+
+	fd = open(filename, O_RDONLY);
+
+	if (fd == -1)
+		return (0);
+
+	bytes_read = read(fd, buf, letters);
+
+	close(fd);
+
+	if (bytes_read == -1)
+		return (0);
+
+        bytes_written = write(1, buf, bytes_read);
+
+	if (bytes_written == -1 || bytes_written != bytes_read)
+		return (0);
+
+	free(buf);
+
+	return (bytes_written);
+ }

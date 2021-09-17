39ee416f3e0fbc4da63190cf5d2713cf7c9846f9
Yakufaith <jaynjaycob@gmail.com>
2021-09-18 01:11:43 +0200

    Contains a function that inserts a node at a given position

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -10,9 +10,7 @@
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *current = *h, *prev;
-	dlistint_t *new_node = malloc(sizeof(dlistint_t));
-
+	dlistint_t *cur = *h, *prev, *new_node = malloc(sizeof(dlistint_t));
 	unsigned int position = 0;
 
 	if (!new_node)
@@ -20,32 +18,36 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 
 	new_node->n = n;
 
+	if (!cur)
+	{
+		new_node->prev = NULL;
+		new_node->next = NULL;
+		*h = new_node;
+		return (new_node);
+	}
 	if (position == idx)
 	{
-		new_node->next = current;
-		current->prev = new_node;
+		new_node->next = cur;
+		cur->prev = new_node;
 		new_node->prev = NULL;
-
 		*h = new_node;
-
 		return (new_node);
 	}
 
-	while (current && (idx != position))
+	while (cur && (idx != position))
 	{
-		prev = current;
-		current = current->next;
+		prev = cur;
+		cur = cur->next;
 		position++;
 	}
-
-	if (!current)
+	if (!cur)
 		return (NULL);
 
-	new_node->next = current;
+	new_node->next = cur;
 	new_node->prev = prev;
 	prev->next = new_node;
-	current->prev = new_node;
-	current = new_node;
+	cur->prev = new_node;
+	cur = new_node;
 
 	return (new_node);
 }

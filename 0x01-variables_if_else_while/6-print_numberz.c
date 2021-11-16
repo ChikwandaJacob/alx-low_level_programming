8ae760cf8bc0fa3483aa371d159f4608c965465a
Yakufaith <jaynjaycob@gmail.com>
2021-11-16 17:10:57 +0200

    Program 7

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -6,11 +6,14 @@
  */
 int main(void)
 {
-int a;
-for (a = 0; a < 10; a++)
-{
-putchar(a + '0');
-}
-putchar('\n');
-return (0);
+	int a;
+
+	for (a = 0; a < 10; a++)
+	{
+		putchar(a + '0');
+	}
+
+	putchar('\n');
+
+	return (0);
 }

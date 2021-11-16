e81cb12dac3db4abe407ef9100e845dd0fb4c0c3
Yakufaith <jaynjaycob@gmail.com>
2021-11-16 17:03:46 +0200

    Program 4 updated

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -7,11 +7,14 @@
  */
 int main(void)
 {
-char a, b;
-for (a = 'a'; a <= 'z'; a++)
-putchar(a);
-for (b = 'A'; b <= 'Z'; b++)
-putchar(b);
-putchar('\n');
-return (0);
+	char a, b;
+
+	for (a = 'a'; a <= 'z'; a++)
+		putchar(a);
+
+	for (b = 'A'; b <= 'Z'; b++)
+		putchar(b);
+	putchar('\n');
+
+	return (0);
 }

b8e1bee6224411b69bd3de12f892d8ef75f80b0e
Yakufaith <jaynjaycob@gmail.com>
2021-11-18 22:31:24 +0200

    Program 8 updated

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -6,14 +6,14 @@
  *
  * Return: returns last value of a number
  */
-int print_last_digit(int n)
+int print_last_digit(long int n)
 {
 	int last_digit;
 
 	if (n < 0)
 		n *= -1;
 
-	last_digit = n % 10;
+	last_digit = (n % 10);
 	_putchar(last_digit + '0');
 
 	return (last_digit);

b29a0e20aa6cc9352a90a07c067a7a5a0be808c9
Yakufaith <jaynjaycob@gmail.com>
2021-11-18 22:17:46 +0200

    Program 8 updated

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,12 +8,9 @@
  */
 int print_last_digit(int n)
 {
-	int last_digit;
-
-	if (n < 0)
+        if (n < 0)
 		n *= -1;
 
-	last_digit = n % 10;
-
-	return (((last_digit) * 10) + last_digit);
+	_putchar(n % 10);
+	return (n % 10);
 }

9866b92c6af625e335496697afbaeb984c836155
Yakufaith <jaynjaycob@gmail.com>
2021-11-18 23:26:16 +0200

    Program 12 updated

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+#include "main.h"
 #include <stdio.h>
 /**
  * print_to_98 - computes absolute value of a number
@@ -9,30 +9,30 @@
 
 void print_to_98(int n)
 {
-int i;
-if (n <= 98)
-{
-for (i = n; i <= 98; i++)
-{
-printf("%d", i);
-if (i < 98)
-{
-putchar(',');
-putchar(' ');
-}
-}
-}
-else
-{
-for (i = n; i >= 98; i--)
-{
-printf("%d", i);
-if (i > 98)
-{
-putchar(',');
-putchar(' ');
-}
-}
-}
-putchar('\n');
+	int i;
+	if (n <= 98)
+	{
+		for (i = n; i <= 98; i++)
+		{
+			printf("%d", i);
+			if (i < 98)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	else
+	{
+		for (i = n; i >= 98; i--)
+		{
+			printf("%d", i);
+			if (i > 98)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
 }
